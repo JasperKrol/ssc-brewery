7b878f00d7191c5e5c92863afc12ca68dd314383
Jasper Krol <jasper.krol@incentro.com>
2023-08-29 10:55:22 +0200

    26. mvcMatcher -> use the url path with param instead of *

--- src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -8,7 +8,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
-class BeerRestControllerIT extends BaseIT {
+public class BeerRestControllerIT extends BaseIT {
 
     @Test
     void findBeers() throws Exception{
@@ -19,6 +19,12 @@ class BeerRestControllerIT extends BaseIT {
     @Test
     void findBeerById() throws Exception{
         mockMvc.perform(get("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findBeerByUpc() throws Exception{
+        mockMvc.perform(get("/api/v1/beerUpc/0631234200036"))
                 .andExpect(status().isOk());
     }
 }
