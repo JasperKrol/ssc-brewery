797338697ecbc099537c8dc4e9847812e9f91126
Jasper Krol <jasper.krol@incentro.com>
2023-09-25 12:46:32 +0200

    39. implement and test ready for own pasword encoder class for updates

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -14,6 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest
 public class BeerControllerIT extends BaseIT{
 
+
     @Test
     void initCreationFormWithSpring() throws Exception {
         mockMvc.perform(get("/beers/new").with(httpBasic("spring", "guru")))
