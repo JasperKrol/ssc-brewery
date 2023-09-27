e194cdd361e3c08f2143eb4c597d3204e9a2822a
Jasper Krol <jasper.krol@incentro.com>
2023-09-27 10:30:40 +0200

    47. Exercise done for parameter url filter

---
+++ src/main/java/guru/sfg/brewery/security/RestUrlAuthFilter.java
@@ -0,0 +1,26 @@
+package guru.sfg.brewery.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by jt on 6/19/20.
+ */
+@Slf4j
+public class RestUrlAuthFilter extends AbstractRestAuthFilter {
+
+    public RestUrlAuthFilter(RequestMatcher requiresAuthenticationRequestMatcher) {
+        super(requiresAuthenticationRequestMatcher);
+    }
+
+
+    protected String getPassword(HttpServletRequest request) {
+        return request.getParameter("apiSecret");
+    }
+
+    protected String getUsername(HttpServletRequest request) {
+        return request.getParameter("apiKey");
+    }
+}
